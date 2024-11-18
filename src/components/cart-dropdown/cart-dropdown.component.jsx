659aa15bff055bc547a5bd037ed3adfdcfa34697
daryl <watanaprasit@gmail.com>
2024-11-19 07:23:27 +0800

    added new cart functionality

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import { useContext } from "react";
+
+import { CartContext } from '../../contexts/cart.context';
+
+import CartItem from "../cart-item/cart-item.component";
 
 import './cart-dropdown.styles.scss';
 
 import Button from '../button/button.component';
 
 const CartDropdown = () => {
+    const {cartItems } = useContext(CartContext);
+
     return (
         <div className="cart-dropdown-cotainer">
             <div className="cart-items">
-            <Button>Go to checkout</Button>
+                {cartItems.map((item) => (
+                    <CartItem key={item.id} cartItem={item} />
+                ))}
             </div>
+            <Button>Go to checkout</Button>
         </div>
     )
 };

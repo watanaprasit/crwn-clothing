56287b96e30f6d343fb2c3e3aa5910175a0fad22
daryl <watanaprasit@gmail.com>
2024-11-24 13:41:22 +0800

    redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
 import { Routes, Route} from "react-router-dom";
+
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth,
+} from './utils/firebase/firebase.utils';
 
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component.jsx";
 import Authentication from "./routes/authentication/authentication.component.jsx"
 import Shop from "./routes/shop/shop.component.jsx"
 import Checkout from "./routes/checkout/checkout.component.jsx";
+import { setCurrentUser } from './store/user/user.action.js';
 
 const App = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      dispatch(setCurrentUser(user));
+    });
+
+    return unsubscribe;
+  }, [dispatch]);
+
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
